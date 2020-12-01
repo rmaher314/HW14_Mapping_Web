637f33f6795ac289fbad57191ea75ff34928a115
Regan <regan_maher@outlook.com>
2020-11-30 18:49:56 -0600

    Got the map to show a varying size of dot based on a variable.

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,33 +1,63 @@
 // Creating map object
+console.log("working")
 var myMap = L.map("map", {
-    center: [40.7128, -74.0059]
-    
-  });
+  center: [36.77, -119.41],
+  zoom: 7
+});
   
   // Adding tile layer
   L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
-    maxZoom: 18,
-    zoomOffset: -1,
-    id: "mapbox/streets-v11",
-    accessToken: API_KEY
-  }).addTo(myMap);
-  
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
+}).addTo(myMap);
+
  // Use this link to get the geojson data.
 var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 // Grabbing our GeoJSON data..
 d3.json(link, function(data) {
     console.log(data);
-
-    for (var i = 0; i < response.length; i++) {
-        var location = response[i].geometry;
     
-        if (location) {
-          L.marker([geometry.coordinates[1], geometry.coordinates[0]]).addTo(myMap);
+    //Create Market clusters
+    //To
+    
+    var earthquakeArray = data.features;
+    var lat,lon, mag;
+    var markers = L.circleMarker;
+
+  //   var circle = L.circle([51.508, -0.11], {
+  //     color: 'red',
+  //     fillColor: '#f03',
+  //     fillOpacity: 0.5,
+  //     radius: 500
+  // }).addTo(mymap);
+
+
+    for (var i = 0; i < earthquakeArray.length; i++) {
+      // Set the data location of the earthquakes to a variable
+      console.log(earthquakeArray[i]);
+      var location = earthquakeArray[i].geometry;
+    
+        if (location != null) {
+          lat = location.coordinates[1];
+          lon = location.coordinates[0];
+          mag = location.coordinates[2];
+          console.log(lat, lon);
+          //var marker = L.marker([lat, lon]).addTo(myMap);
+
+          var circle = L.circleMarker([lat, lon], {
+            color: 'red',
+            radius: mag
+          }).addTo(myMap);
         }
       }
+      
+
+    });
     // Creating a geoJSON layer with the retrieved data
 //     geoJson = L.geoJson(data, {
 //       // Style for each feature (in this case a neighborhood)
@@ -65,4 +95,3 @@ d3.json(link, function(data) {
 //       }
 //     }).addTo(myMap);
 //   });
-  
