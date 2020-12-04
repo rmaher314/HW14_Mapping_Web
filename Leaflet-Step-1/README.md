df083e7b95b29a14e71b830c07ebf7fd14e9916b
Regan <regan_maher@outlook.com>
2020-12-04 13:46:01 -0600

    Final commit for assignment.

--- Leaflet-Step-1/README.md
+++ Leaflet-Step-1/README.md
@@ -1,1 +1,7 @@
 # HW14_Mapping_Web
+
+Documents for this Homework:
+1.  index.html
+2.  style.css
+3.  logic.js
+
--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -29,12 +29,7 @@ d3.json(link, function(data) {
     var lat,lon, dep, mag, descriptor, hex, place, depthColor;
     var markers = L.circleMarker;
 
-  //   var circle = L.circle([51.508, -0.11], {
-  //     color: 'red',
-  //     fillColor: '#f03',
-  //     fillOpacity: 0.5,
-  //     radius: 500
-  // }).addTo(mymap);
+
 
 
     for (var i = 0; i < earthquakeArray.length; i++) {
@@ -49,69 +44,67 @@ d3.json(link, function(data) {
           dep = location.coordinates[2];
           mag = earthquakeArray[i].properties.mag;
           place = earthquakeArray[i].properties.place;
-          descriptor = mag + " " + place + " " + dep;
+          descriptor = "Magnitude: " + mag + " Location: " + place + " Depth: " + dep;
           hex = dep.toString(16);
           console.log(lat, lon, dep, mag, hex);
           //var marker = L.marker([lat, lon]).addTo(myMap);
-          if (dep <10) {
+          if (dep <5) {
             depthColor = "green";
           }
-            else if (dep <20) {
-            depthColor = "red";
+            else if (dep <10) {
+            depthColor = "cyan";
+          }
+            else if (dep <50) {
+            depthColor = "blue";
+          }
+            else if (dep <100) {
+            depthColor = "purple";
+          }
+            else if (dep <150) {
+            depthColor = "pink";
+          }
+            else if (dep <200) {
+            depthColor = "yellow";
+          }
+            else if (dep <250) {
+            depthColor = "orange";
           }
             else {
-              depthColor = "purple";
+              depthColor = "red";
             }
           var circle = L.circleMarker([lat, lon], {
             color: depthColor,
             fillColor: depthColor,
-            radius: mag * 10,
+            radius: mag * 5,
             fillOpacity: 0.5
           }).bindPopup(descriptor).addTo(myMap);
 
-          // var marker = L.marker(lat, lon{
-          //   icon: dep
-          // })
+        
         }
 
       }
       
 
+   
+       
+      var legend = L.control({position: 'bottomleft'});
+      legend.onAdd = function (map) {
+  
+      var div = L.DomUtil.create('div', 'info legend');
+      var labels = ['<strong>Depth</strong>']
+      var categories = ['0 - 4.9 depth','5 - 9.9 depth','10 - 49.9 depth','50 - 99.9 depth','100 - 149.9 depth','150 - 199.9 depth','200 - 249.9 depth','250 or deeper'];
+      var colors =['green','cyan','blue','purple','pink','yellow','orange','red']
+      for (var i = 0; i < categories.length; i++) {
+  
+              div.innerHTML += 
+              labels.push(
+                  '<i style="background:' + colors[i] + '!Important;"></i> ' +
+              (categories[i] ? categories[i] : '+'));
+  
+          }
+          div.innerHTML = labels.join('<br>');
+      return div;
+      };
+      legend.addTo(myMap);
+
     });
-    // Creating a geoJSON layer with the retrieved data
-//     geoJson = L.geoJson(data, {
-//       // Style for each feature (in this case a neighborhood)
-//       style: function(feature) {
-//         return {
-//           color: "white",
-//           // Call the chooseColor function to decide which color to color our neighborhood (color based on borough)
-//           fillColor: chooseColor(feature.properties.borough),
-//           fillOpacity: 0.5,
-//           weight: 1.5
-//         };
-//       },
-//       // Called on each feature
-//       onEachFeature: function(feature, layer) {
-//         // Setting various mouse events to change style when different events occur
-//         layer.on({
-//           // On mouse over, make the feature (neighborhood) more visible
-//           mouseover: function(event) {
-//             layer = event.target;
-//             layer.setStyle({
-//               fillOpacity: 0.9
-//             });
-//           },
-//           // Set the features style back to the way it was
-//           mouseout: function(event) {
-//             geoJson.resetStyle(event.target);
-//           },
-//           // When a feature (neighborhood) is clicked, fit that feature to the screen
-//           click: function(event) {
-//             myMap.fitBounds(event.target.getBounds());
-//           }
-//         });
-//         // Giving each feature a pop-up with information about that specific feature
-//         layer.bindPopup("<h1>" + feature.properties.neighborhood + "</h1> <hr> <h2>" + feature.properties.borough + "</h2>");
-//       }
-//     }).addTo(myMap);
-//   });
